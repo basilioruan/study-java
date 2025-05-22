468076a1c1466485609e6a654b0ec4064e48718c
basilioruan <ruan_basilio@outlook.com>
2025-05-22 20:24:23 -0300

    init: creating project

---
+++ jasper/src/test/java/com/study/java/jasper/JasperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.java.jasper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
