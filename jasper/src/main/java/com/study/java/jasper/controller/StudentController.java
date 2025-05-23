db06c1a15785631257a04168e1470bd6a70d53ac
basilioruan <ruan_basilio@outlook.com>
2025-05-22 22:55:13 -0300

    wip: init generation of jasper report structure

--- jasper/src/main/java/com/study/java/jasper/controller/StudentController.java
+++ jasper/src/main/java/com/study/java/jasper/controller/StudentController.java
@@ -1,8 +1,11 @@
 package com.study.java.jasper.controller;
 
+import com.study.java.jasper.infrastructure.model.Student;
 import com.study.java.jasper.services.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +17,14 @@ public class StudentController {
 
     private final StudentService studentService;
 
-    @GetMapping
-    public void generateStudentCertification(@RequestParam("id") Long id) {
-        this.studentService.generateStudentCertification(id);
+    @GetMapping("/generate-certificate")
+    public void generateStudentCertificate(@RequestParam("id") Long id) {
+        this.studentService.generateStudentCertificate(id);
+    }
+
+    @PostMapping("/generate-certificate")
+    public void generateStudentCertificate(@RequestBody Student student) {
+        this.studentService.generate(student);
     }
 
 }
--- jasper/src/main/java/com/study/java/jasper/services/StudentService.java
+++ jasper/src/main/java/com/study/java/jasper/services/StudentService.java
@@ -3,16 +3,84 @@ package com.study.java.jasper.services;
 import com.study.java.jasper.infrastructure.model.Student;
 import com.study.java.jasper.infrastructure.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class StudentService {
 
+    public static final String CERTIFICATES = "classpath:jasper/certificates";
+
+    public static final String JRXML_FILE = "cert.jrxml";
+
+    public static final String DESTINATION_PDF = "C:\\jasper-report\\";
+
+    public static final String DIRECTORY_SAVED_CERTIFICATES = "saved-certificates";
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(StudentService.class);
+
     private final StudentRepository studentRepository;
 
-    public void generateStudentCertification(Long id) {
+    public void generateStudentCertificate(Long id) {
         Student student = this.studentRepository.findById(id).orElseThrow();
+        this.generate(student);
+    }
+
+    public void generate(Student student) {
+        Map<String, Object> params = this.buildParams(student);
+
+        try {
+            String path = CERTIFICATES + JRXML_FILE;
+            String folderPdfDirectory = this.getSaveDirectory(DIRECTORY_SAVED_CERTIFICATES);
+            String absolutePath = this.getAbsolutePath(path);
+            JasperReport report = JasperCompileManager.compileReport(path);
+            LOGGER.info("Report compilado: {}", absolutePath);
+            JasperPrint print = JasperFillManager.fillReport(report, params, new JREmptyDataSource());
+            LOGGER.info("Jasper print");
+            JasperExportManager.exportReportToPdfFile(print, folderPdfDirectory);
+        } catch (JRException | FileNotFoundException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private Map<String, Object> buildParams(Student student) {
+        return Map.of(
+            "nome", student.getName(),
+            "curso", student.getCourse(),
+            "cargaHoraria", student.getWorkLoad(),
+            "dataInicioCurso", student.getInitialCourseDate(),
+            "dataFimCurso", student.getFinishCourseDate()
+        );
+    }
+
+    private String getAbsolutePath(String path) throws FileNotFoundException {
+        return ResourceUtils.getFile(path).getAbsolutePath();
+    }
+
+    private String getSaveDirectory(String directoryName) {
+        this.createDirectory(directoryName);
+        return DESTINATION_PDF + directoryName.concat(".pdf");
+    }
+
+    private void createDirectory(String name) {
+        File dir = new File(name);
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
     }
 
 }
